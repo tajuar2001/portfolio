05c4a00b5d00bd6ef89a2e5efeaacc71a1784753
tajuarb <tajuarb@umich.edu>
2023-11-29 23:15:39 -0500

    added item to nav bar

--- src/components/home/Books.jsx
+++ src/components/home/Books.jsx
@@ -4,7 +4,7 @@ import './books.css';
 const Books = ({ books }) => {
   return (
     <section id="books">
-      <h2 className="display-4 pb-5 text-center">What I'm Reading Now</h2>
+      <h2 className="display-4 pb-3 text-center">What I'm Reading Now</h2>
       <div className="books-container">
         {books.map((book, index) => (
           <div key={index} className="book">
